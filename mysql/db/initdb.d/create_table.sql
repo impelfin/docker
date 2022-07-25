057b53ca79ada4bb67ac311cb6db8d0752dfca4c
JunHyeon Mun <impelfin@gmail.com>
2022-07-25 21:46:35 +0900

    .

--- mysql/db/initdb.d/create_table.sql
+++ mysql/db/initdb.d/create_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE departments (
-    `id`        INT          NOT NULL AUTO_INCREMENT,
-    `name`      VARCHAR(25)  NOT NULL UNIQUE,
-    `priority`  TINYINT      NOT NULL,
+    'id'        INT          NOT NULL AUTO_INCREMENT,
+    'name'      VARCHAR(25)  NOT NULL UNIQUE,
+    'priority'  TINYINT      NOT NULL,
     PRIMARY KEY (id)
 );
