020acd40bf0a6e929978d310c95c956fb8f40e4a
JunHyeon Mun <impelfin@gmail.com>
2022-07-26 20:39:56 +0900

    .

---
+++ apps/fastapi2/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/users/{user_id}")
+def get_user(user_id):
+    return {"user_id":user_id}
+
+    if __name__ == "__main__":
+        uvicorn.run(app, host="0.0.0.0", port=8000)
